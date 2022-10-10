5bb7e72bb11c0eff96b41fd9269706c2080f20e2
David OC <davidherzlos@gmail.com>
2022-10-09 21:40:48 -0500

    dynamic attributes

--- src/Tutorial.svelte
+++ src/Tutorial.svelte
@@ -1,6 +1,11 @@
 <script>
+    import svelteLogo from './assets/svelte.svg';
     let name = 'Svelte';
+    let alt = name + ' cool logo.';
 </script>
 
 <!-- Adding simple data -->
 <h1>Hello {name}, i need the 'language' prop to be here</h1>
+
+<!-- Dynamic attributes -->
+<img src={svelteLogo} {alt}>
