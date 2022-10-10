acff424ce358ed3e7c768ebce05c5be9e92d265c
David OC <davidherzlos@gmail.com>
2022-10-09 21:09:49 -0500

    Initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { dataset_dev } from "svelte/internal";
+
+
+
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
